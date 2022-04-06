90bff9b8d53598c53eb2926fe9b299472d9a52f4
Syed Ameen Quadri <ameenquadri2000@gmail.com>
2022-04-06 11:36:12 -0400

    Front end CRUD filtered GET

--- Shopium-Client/src/app/app.component.ts
+++ Shopium-Client/src/app/app.component.ts
@@ -12,26 +12,60 @@ import { Item } from './Item/items';
 export class AppComponent implements OnInit {
   title = 'Shopium-Client';
   public items: Item[] = [];
+  public searchQuery: string = "";
 
   constructor(private itemService: ItemService) { }
 
   // executes on initialization
   ngOnInit() {
-    this.getItems();
+    this.getAllItems();
   }
 
-  public getItems(): void {
+  /**
+   * Get all items from the total items list that relate to the search query
+   */
+  public getSearchItems(searchQuery: string): void {
+    console.log(searchQuery);
+    this.items = []; // clear out
     this.itemService.getItems().subscribe(response => {
-
       console.log(response);
+      //const keys = response.headers.keys();
+      //const mapper = keys.map(key => `${key}:${response.headers.get(key)}`);
+      for (const item of response?.body._embedded.itemList) {
+        console.log(item.name.includes(searchQuery));
+        console.log(item.description.includes(searchQuery));
+        console.log(item.type.includes(searchQuery));
 
-      const keys = response.headers.keys();
-      const mapper = keys.map(key => `${key}:${response.headers.get(key)}`);
+        if (item.name?.includes(this.searchQuery) ||
+          item.description?.includes(this.searchQuery) ||
+          item.type?.includes(this.searchQuery)) {
+          this.items.push(item);
+        }
+      }
+    });
+  }
 
+  public getAllItems(): void {
+    this.items = []; // clear out
+    this.itemService.getItems().subscribe(response => {
+      console.log(response);
+      //const keys = response.headers.keys();
+      //const mapper = keys.map(key => `${key}:${response.headers.get(key)}`);
       for (const data of response?.body._embedded.itemList) {
         this.items.push(data);
       }
-
     });
+  }
+
+  /**
+   * depends on getItems()
+   */
+  public getSingularItem(iid: number): Item | undefined {
+    for (const item of this.items) {
+      if (item.iid == iid) {
+        return item;
+      }
+    }
+    return undefined;
   }
 }
